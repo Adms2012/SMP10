129dc322885bdc1fa13f8c0bdd2fda91eb61feb3
Durendal Huynh <durendalhuynh@gmail.com>
2013-12-17 16:39:16 -0600

    Changes made by Durendal Huynh (durendalhuynh@gmail.com).

---
+++ SMP10/scripts/cartView.js
@@ -0,0 +1,79 @@
+(function (global) { 
+    
+    var _view,
+        app = global.app = global.app || {};    
+    
+    var _scrollToTopIfTooFewItemsInCart = function () {
+        if(_view.scrollerContent.height() < _view.scroller.element.height()) {
+            utils.scrollViewToTop(_view.element);
+        }
+    };
+
+    var onRemove = function (clickEvt) {
+            var element = clickEvt.sender.element;
+            var li = element.parents("li");
+            var uid = li.data("uid");
+
+            var animation = {
+                "opacity": 0
+            };
+
+            li.animate(animation,
+                300,
+                function () {
+                    cart.items.remove(cart.items.getByUid(uid));
+                    _scrollToTopIfTooFewItemsInCart();
+                });
+        },
+
+        onToggleDeleteMode = function (evt) {
+            var element = evt.sender.element;
+            var li = element.parents("li");
+            var uid = li.data("uid");
+            var cartItem = cart.items.getByUid(uid);
+
+            cartItem.set("deleteMode", !cartItem.deleteMode);
+        },
+
+        viewModel = kendo.observable({
+            items: app.cart.items,
+            cartHasItems: false,
+            cart: app.cart,
+
+            isAndroid: function () {
+                return $("body").hasClass("km-android");
+            },
+
+            serviceItemPrice: function (cartItem) {
+                return kendo.toString(parseFloat(cartItem.serviceItem.get("serviceItemUnitPrice")), "c");
+            },
+            serviceItemArtUrl: function (cartItem) {
+                return ""; //config.serverUrl + cartItem.serviceItem.get("serviceItemArtUrl");
+            },
+            serviceItemSubtotal: function (cartItem) {
+                return kendo.toString(parseFloat(cartItem.serviceItem.get("serviceItemUnitPrice")) * cartItem.get("qty"), "c");
+            }
+        }),
+
+        init = function (initEvt) {
+            _view = initEvt.view
+            viewModel.set("cartHasItems", cart.items.view().length > 0);
+            cart.items.bind("change", function () {
+                viewModel.set("cartHasItems", cart.items.view().length > 0);
+            });
+        },
+
+        layoutShow = function () {
+            utils.updateCartBadges($, cart);
+        };
+
+
+    app.cartView = {
+        init: init,
+        layoutShow: layoutShow,
+        onRemove: onRemove,
+        onToggleDeleteMode: onToggleDeleteMode,
+        viewModel: viewModel    
+    };
+    
+})(window);        
